b6ea8688021341534c8f24099c17100ccc5d68d3
park.saerom <park.jungkyu@boi.jp>
2020-11-24 11:40:09 +0900

    First Commit

---
+++ Assets/DefaultTextureForUI/Editor/DefaultTextureSetter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CustomEditor(typeof(CanvasRenderer), true)]
+[CanEditMultipleObjects]
+public class DefaultTextureSetter : Editor
+{
+    public Sprite defaultSprite;
+    
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        var script = (Component)target;
+        var imageComponent = script.GetComponent<Image>();
+        if (imageComponent != null && imageComponent.sprite == null)
+        {
+            imageComponent.sprite = defaultSprite;
+            return;
+        }
+
+        var rawImageComponent = script.GetComponent<RawImage>();
+        if (rawImageComponent != null && rawImageComponent.texture == null)
+        {
+            rawImageComponent.texture = defaultSprite.texture;
+        }
+    }
+}
